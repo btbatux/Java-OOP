d176bc8d6e31c0b0e08d0f021eff9c97b7df0e9f
btbatux <rct-batuhan@hotmail.com>
2023-11-26 20:47:46 +0300

    Customer Class
    Customer Class Example

---
+++ Classes/CustomerManager.java
@@ -0,0 +1,21 @@
+package Classes;
+
+public class CustomerManager {
+
+      public void Add() {
+            System.out.println("Müşteri Eklendi..");
+      }
+
+      public void Remove() {
+            System.out.println("Müşteri Silindi");
+      }
+
+      public void Update() {
+            System.out.println("Müşteri Güncellendi");
+      }
+
+      //
+      public void getir(String ad) {
+            System.out.println("Adınız: " + ad);
+      }
+}
