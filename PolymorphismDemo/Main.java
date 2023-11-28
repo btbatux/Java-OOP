4a952aeaac7bfa264c338bc106b81fb0abe98f9c
btbatux <rct-batuhan@hotmail.com>
2023-11-28 20:37:13 +0300

    Java-OOP
    Polymorphism Logger Example

---
+++ PolymorphismDemo/Main.java
@@ -0,0 +1,18 @@
+package PolymorphismDemo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // BaseLogger[] loggers = new BaseLogger[] { new FileLogger(), new
+        // EmailLogger(), new DatabaseLogger(),
+        // new BaseLogger(),new ConsoleLogger() };
+
+        // for (BaseLogger logger : loggers) {
+        // logger.log("Test Mesajı...");
+        // }
+
+        CustomerManager customerManager = new CustomerManager(new DatabaseLogger());
+
+    }
+
+}
