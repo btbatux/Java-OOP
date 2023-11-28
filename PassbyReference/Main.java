1067ab9a11044752f9e95302ea243aa9573a7d6f
btbatux <rct-batuhan@hotmail.com>
2023-11-28 22:40:16 +0300

    PassByReference
    PassByReference

--- PassbyReference/Main.java
+++ PassbyReference/Main.java
@@ -6,8 +6,8 @@ public class Main {
         PassByReference reference = new PassByReference();
         reference.number2 = 7;
 
-        System.out.println("Number2 value: " + reference.number2);
+        System.out.println("Number2 first value: " + reference.number2);
         reference.change(reference);
-        System.out.println("Number2 value: " + reference.number2);
+        System.out.println("Number2 2nd value: " + reference.number2);
     }
 }
--- PassbyReference/PassByReference.java
+++ PassbyReference/PassByReference.java
@@ -5,7 +5,8 @@ class PassByReference {
     public int number2;
 
     public void change(PassByReference reference) {
-        reference = new PassByReference();
+
+        // reference = new PassByReference();
 
         reference.number2 = reference.number2 + 3;
         // reference.number2 = 3;
