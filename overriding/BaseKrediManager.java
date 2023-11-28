832896496014d7152685abc29dbd47e3c7a6434b
btbatux <rct-batuhan@hotmail.com>
2023-11-28 22:33:18 +0300

    Update BaseKrediManager.java

--- overriding/BaseKrediManager.java
+++ overriding/BaseKrediManager.java
@@ -3,7 +3,7 @@ package overriding;
 public class BaseKrediManager {
 
     // kredi hesaplama fonk.
-    public final double hesapla(double tutar) {
+    public double hesapla(double tutar) {
         return tutar * 1.18;
     }
 }
